e529cd2a9926a7a8439b0846dc370f9be866ff49
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2023-09-02 20:58:37 -0300

    chore: tailwindcss init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  // Arquivos que podem ter classes do tailwind a gente configura aqui
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        paulo: "#8257e6",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
