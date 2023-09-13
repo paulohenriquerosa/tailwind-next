3bc60783ec7ac37ba20107d52a8136c3dfacca9f
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2023-09-13 13:27:35 -0300

    chore: comments code

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -18,15 +18,17 @@ import { Input } from '../Input'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-3  border-r border-zinc-200 px-5 py-8">
+    <aside className="bottom-o fixed left-0 right-0 top-0 z-20 flex h-screen flex-col gap-6 border-r border-zinc-200 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
 
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
+      {/* <Input.Root> */}
+      {/*   <Input.Prefix> */}
+      {/*     <Search className="h-5 w-5 text-zinc-500" /> */}
+      {/*   </Input.Prefix> */}
+      {/*   <Input.Control placeholder="Search" /> */}
+      {/* </Input.Root> */}
+
+      <div className="h-px bg-zinc-200" />
 
       <div className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
@@ -42,7 +44,7 @@ export function Sidebar() {
           <NavItem title="Support" icon={LifeBuoy} />
           <NavItem title="Settings" icon={Cog} />
         </nav>
-        <UsedSpaceWidget />
+        {/* <UsedSpaceWidget /> */}
       </div>
       <div className="h-px bg-zinc-200" />
       <Profile />
